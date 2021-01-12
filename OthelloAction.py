ee27cc49bc7a56fcdcb02e30ec7b969ba03a063a
KLag-SF <k.lagopus.sf19@gmail.com>
2021-01-13 03:37:52 +0900

    VERSION Beta3

--- OthelloAction.py
+++ OthelloAction.py
@@ -1,8 +1,8 @@
 import random
-import AB_kai as ab
+import AlphaBetagit as ab
 import sys
 
-DEPTH = 3
+DEPTH = 6
 SIZE = 8
 
 """
@@ -21,6 +21,8 @@ def getAction(board, moves):
 	puttable = len(sorted_move)
 	move_candidate = sorted_move[-1]
 
+	print("Candidate:" + str(move_candidate))
+
 	corner = [[0, 0], [0 ,7], [7, 0], [7, 7]]
 	x_point = [[1, 1], [1, 6], [6, 1], [6, 6]]
 	c_point = [ [1, 0], [0, 6], [6, 0], [0, 1],
@@ -35,22 +37,46 @@ def getAction(board, moves):
 		for p in sorted_move:
 			if p not in x_point:
 				move_candidate = p
-				print("candidate updated to " + str(p))
-				is_c = (move_candidate in c_point) and (puttable >= 2)
-				print("Is candidate C? " + str(is_c))
+				print("Candidate updated to " + str(p))
+				is_x = (move_candidate in x_point) and (puttable >= 2)
+				if is_x:
+					continue
+				else:	
+					is_c = (move_candidate in c_point) and (puttable >= 2)
+					print("Is candidate C? " + str(is_c))
+					break
+			else:
+				sorted_move.remove(p)
+				print("Removed X from puttable table. :" + str(p))
+				is_x = (move_candidate in x_point) and (puttable >= 2)
 
-	elif is_c:
+	if is_c:
 		for p in sorted_move:
 			if p not in c_point:
-				return p
-	else:
-		for i in range(len(sorted_move)):
+				move_candidate = p
+				print("Candidate updated to " + str(p))
+				is_c = (move_candidate in c_point) and (puttable >= 2)
+				is_x = (move_candidate in x_point) and (puttable >= 2)
+				if is_c:
+					continue
+				elif is_x:
+					continue
+				else:
+					break
+	
+	for i in range(len(sorted_move)):
+		try:
 			p = sorted_move[i]
 			print(p)
 			print("Can AI get corner? " + str(p in corner))
+			is_x = (move_candidate in x_point) and (puttable >= 2)
 			if p in corner:
 				return p
+			elif is_x:
+				sorted_move.remove(p)
 			else:
 				continue
+		except IndexError:
+			break
 
-		return move_candidate
+	return move_candidate
